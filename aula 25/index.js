4c76af4aef2bfe0b141706544a2095693b280fbe
Abner-Git-Hub <abnersilva-2002@outlook.com>
2026-08-14 21:02:31 -0300

    Atualizando

--- aula 25/index.js
+++ aula 25/index.js
@@ -14,7 +14,7 @@ const num2 = '10';  // String
 // O operador '==' Diz que o num1 e o num2 são iguais porem eles não são iguais, o num1 é um number e o num2 é uma string;
 // por isso nao é usado o operador '==';
 
-const comp = num1 == num2 ;
+const comp = num1 === num2 ;
 
 console.log(comp);
 
